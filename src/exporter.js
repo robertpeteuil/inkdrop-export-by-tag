f44bf4adfe8a0d66223bc008e623bd55e4303947
Robert Peteuil <robert@peteuil.com>
2020-11-24 16:29:47 -0700

    feat: add async file write and overwrite protect

--- src/exporter.js
+++ src/exporter.js
@@ -29,9 +29,9 @@ function notify(level, message, details, critialErr) {
   }
 }
 
-async function getTag(db, tagName) {
+async function getTag(tags, tagName) {
   try {
-    const tags = db.tags;
+    // const tags = db.tags;
     const tagMeta = await tags.findWithName(tagName);
     return tagMeta._id;
   } catch (err) {
@@ -62,10 +62,11 @@ export async function exportTaggedNotes(htmlMode, tagName) {
 
   const pConfig = inkdrop.config.get('export-by-tag');
 
-  pConfig.mode = (htmlMode) ? "html" : "md"
+  // pConfig.mode = (htmlMode) ? "html" : "md"
+  pConfig.htmlMode = htmlMode
 
   const db = await inkdrop.main.dataStore.getLocalDB();
-  const tagId = await getTag(db, tagName);
+  const tagId = await getTag(db.tags, tagName);
   var noteList = await getNotes(db.notes, tagId);
 
   if (pConfig.exportDir) {
@@ -81,25 +82,18 @@ export async function exportTaggedNotes(htmlMode, tagName) {
   }
 
   for (var i = 0; i < noteList.length; i++) {
-
       var notePath = '';
       if (pConfig.dirStruct) {
         let bookArray = [];
         bookArray = await createBookArray(noteList[i].bookId, db.books, bookArray);
         notePath = bookArray.join('/');
       }
-
       await exportNote(noteList[i], notePath, pConfig);
- 
   }
   
-  if (i == 1) {
-    notify('Info', `Exported Note with tag:${tagName}`, `${i} Note exported to ${pConfig.exportPath}`);
-  } else if (i > 1) {
-    notify('Info', `Exported Notes with tag:${tagName}`, `${i} Notes exported to ${pConfig.exportPath}`);
-  } else {
-    notify('Error', 'No Notes Exported', `Unknown Error`);
-  }
+  const notesP = (i > 1) ? "Notes" : "Note"
+  notify('Info', `Exported ${notesP} with tag:${tagName}`, `${i} ${notesP} exported to ${pConfig.exportPath}`);
+
   return `${i} Notes exported to ${pConfig.exportPath}`;
 }
 
@@ -121,10 +115,16 @@ async function exportNote(note, notePath, pConfig) {
     const fileDir = path.join(pConfig.exportPath, notePath);
     await checkDir(fileDir);
 
-    var fileExt = pConfig.mode;
+    // var fileExt = pConfig.mode;
+    var fileExt;
     var fileBody;
 
-    if (pConfig.mode == "md") {
+    if (pConfig.htmlMode) {
+      const tempBody = '# ' + note.title + '\n\n' + note.body;
+      const rawBody = await renderHTML(tempBody);
+      fileBody = await replaceHTMLImagesWithDataURI(rawBody);
+      fileExt = "html"
+    } else {
       let body = note.body
       const uris = body.match(/inkdrop:\/\/file:[^) "']*/g) || []
       if (uris.length > 0) {
@@ -133,27 +133,76 @@ async function exportNote(note, notePath, pConfig) {
         body = await replaceImages(body, picDir, fileDir)
       }
       fileBody = '# ' + note.title + '\n\n' + body;
-    } else {
-      const tempBody = '# ' + note.title + '\n\n' + note.body;
-      const rawBody = await renderHTML(tempBody);
-      fileBody = await replaceHTMLImagesWithDataURI(rawBody);
+      fileExt = "md"
     }
 
+    // if (pConfig.mode == "md") {
+    //   let body = note.body
+    //   const uris = body.match(/inkdrop:\/\/file:[^) "']*/g) || []
+    //   if (uris.length > 0) {
+    //     const picDir = path.join(fileDir, "pics")
+    //     await checkDir(picDir);
+    //     body = await replaceImages(body, picDir, fileDir)
+    //   }
+    //   fileBody = '# ' + note.title + '\n\n' + body;
+    // } else {
+    //   const tempBody = '# ' + note.title + '\n\n' + note.body;
+    //   const rawBody = await renderHTML(tempBody);
+    //   fileBody = await replaceHTMLImagesWithDataURI(rawBody);
+    // }
+
+    // console.log("about to start writing file")
+    // new method
     const sanitizedTitle = sanitize(note.title, { replacement: '-' });
-    const fileName = `${sanitizedTitle}.${fileExt}`;
-    const filePath = path.join(fileDir, fileName);
+    await writeToFile(sanitizedTitle, fileExt, fileDir, fileBody, pConfig.dateType, note);
 
-    try {
-      fs.writeFileSync(filePath, fileBody);
-    } catch (err) {
-      notify('Error', 'Error writing file', err.message, true);
-    }
-    if (pConfig.dateType == "Create") {
-      touch.sync(filePath, { time: new Date(note.createdAt) });
-    } else if (pConfig.dateType == "Update") {
-      touch.sync(filePath, { time: new Date(note.updatedAt) });
-    }
+    // original method
+    // const sanitizedTitle = sanitize(note.title, { replacement: '-' });
+    // var fileName = `${sanitizedTitle}.${fileExt}`;
+    // var filePath = path.join(fileDir, fileName);
+    // try {
+    //   fs.writeFileSync(filePath, fileBody);
+    // } catch (err) {
+    //   notify('Error', 'Error writing file', err.message, true);
+    // }
+
+    // Set optional create and modify date on file
+    // if (pConfig.dateType == "Create") {
+    //   touch.sync(filePath, { time: new Date(note.createdAt) });
+    // } else if (pConfig.dateType == "Update") {
+    //   touch.sync(filePath, { time: new Date(note.updatedAt) });
+    // } else if (pConfig.dateType == "Both") {
+    //   fs.utimesSync(filePath, new Date(note.updatedAt), new Date(note.createdAt));
+    //   touch.sync(filePath, { time: new Date(note.updatedAt) });
+    // }
   }
+}
+
+async function writeToFile(fileTitle, fileExt, fileDir, fileBody, dateType, note) {
+  var fileName = `${fileTitle}.${fileExt}`;
+  var filePath = path.join(fileDir, fileName);
+
+  // flag "wx" = write without overwrite, "w" write okay to overwrite
+  fs.writeFile(filePath, fileBody, { flag: "wx" }, function(err) {
+    if (err) {
+      // console.log("file " + fileName + " already exists, testing next");
+      const datestr = new Date().toISOString().replace(/-/g, '').replace('.', '').replace(/:/g, '').replace('T','-').replace('Z','')
+      fileTitle = fileTitle + '-' + datestr;
+      writeToFile(fileTitle, fileExt, fileDir, fileBody, dateType, note);
+    }
+    else {
+      // console.log("Successfully written " + fileTitle);
+      // Set optional create and modify date on file
+      if (dateType == "Create") {
+        touch.sync(filePath, { time: new Date(note.createdAt) });
+      } else if (dateType == "Update") {
+        touch.sync(filePath, { time: new Date(note.updatedAt) });
+      } else if (dateType == "Both") {
+        fs.utimesSync(filePath, new Date(note.updatedAt), new Date(note.createdAt));
+        touch.sync(filePath, { time: new Date(note.updatedAt) });
+      }
+    }
+});
 }
 
 async function checkDir(dirName) {
